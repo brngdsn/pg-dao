cdd3d7887eafe76d988181e87e57ef0bbe82619b
Braun Goodson <brn.gdsn@gmail.com>
2023-10-07 11:18:03 -0400

    backwards compatible parameterize inputs (#4)
    * parameterize inputs
    
    * bump

--- src/pg-dao.js
+++ src/pg-dao.js
@@ -24,38 +24,67 @@ module.exports = class PostgresDAO {
     }
   }
 
-  async create (table, data) {
+  async create(table, data) {
     const fields = Object.keys(data).join(',');
-    const values = Object.values(data).map(value => `${value}`).join(',');
-    const sql = `INSERT INTO ${table} (${fields}) VALUES (${values}) RETURNING *`;
-    console.log({ sql })
-    const { rows } = await this.pool.query(sql);
+    const placeholders = Object.keys(data).map((_, index) => `$${index + 1}`).join(',');
+    const values = Object.values(data);
+    const sql = `INSERT INTO ${table} (${fields}) VALUES (${placeholders}) RETURNING *`;
+    console.log({ sql, values });
+    const { rows } = await this.pool.query(sql, values);
     return rows[0];
   }
 
-  async read (table, select = { '*': true }, where = { }, limit = 25) {
+  async read(table, select = { '*': true }, where = {}, limit = 25) {
     const select_keys = Object.keys(select).join(',');
-    const where_clause = Object.keys(where).map(field => `${field} = ${where[field]}`).join(' and ');
-    const sql = `SELECT ${select_keys} FROM ${table} ${where_clause.length ? `WHERE ${where_clause}` : ``} LIMIT ${limit}`;
-    console.log({ sql })
-    const { rows } = await this.pool.query(sql);
+    const whereKeys = Object.keys(where);
+    const wherePlaceholders = whereKeys.map((_, index) => `${whereKeys[index]} = $${index + 1}`).join(' and ');
+    const values = Object.values(where);
+
+    let sql = `SELECT ${select_keys} FROM ${table}`;
+    if (wherePlaceholders.length) {
+        sql += ` WHERE ${wherePlaceholders}`;
+    }
+    
+    values.push(limit);
+    sql += ` LIMIT $${values.length}`;
+    
+    console.log({ sql, values });
+    const { rows } = await this.pool.query(sql, values);
     return rows;
   }
 
-  async update (table, where = { id: null }, data) {
-    const where_clause = Object.keys(where).map(field => `${field} = ${where[field]}`).join(' and ');
-    const fields = Object.keys(data).map(field => `${field} = ${data[field]}`).join(',');
-    const sql = `UPDATE ${table} SET ${fields} WHERE ${where_clause} RETURNING *`;
-    console.log({ sql })
-    const { rows } = await this.pool.query(sql);
+  async update(table, where = { id: null }, data) {
+    const whereKeys = Object.keys(where);
+    const dataKeys = Object.keys(data);
+    
+    // Create placeholders for SET and WHERE clauses
+    const setPlaceholders = dataKeys.map((_, index) => `${dataKeys[index]} = $${index + 1}`).join(',');
+    const wherePlaceholders = whereKeys.map((_, index) => `${whereKeys[index]} = $${dataKeys.length + index + 1}`).join(' and ');
+
+    const sql = `UPDATE ${table} SET ${setPlaceholders} WHERE ${wherePlaceholders} RETURNING *`;
+
+    // Combine values for SET and WHERE clauses
+    const values = [...Object.values(data), ...Object.values(where)];
+
+    console.log({ sql, values });
+    const { rows } = await this.pool.query(sql, values);
     return rows[0];
   }
 
-  async delete (table, where = { id: null }) {
-    const where_clause = Object.keys(where).map(field => `${field} = ${where[field]}`).join(' and ');
-    const sql = `DELETE FROM ${table} WHERE ${where_clause} RETURNING *`;
-    console.log({ sql })
-    const { rows } = await this.pool.query(sql);
+  async delete(table, where = { id: null }) {
+    const whereKeys = Object.keys(where);
+    
+    // Create placeholders for the WHERE clause
+    const wherePlaceholders = whereKeys.map((_, index) => `${whereKeys[index]} = $${index + 1}`).join(' and ');
+
+    const sql = `DELETE FROM ${table} WHERE ${wherePlaceholders} RETURNING *`;
+
+    // Extract values for the WHERE clause
+    const values = Object.values(where);
+
+    console.log({ sql, values });
+    const { rows } = await this.pool.query(sql, values);
     return rows[0];
   }
+
 }
